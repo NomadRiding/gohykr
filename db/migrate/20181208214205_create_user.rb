2eeac1ab0625d9fe9786a517d084dd7a42cb3968
Maya Kano <mayakanoweb@gmail.com>
2018-12-08 16:43:53 -0500

    new migrate file

---
+++ db/migrate/20181208214205_create_user.rb
@@ -0,0 +1,11 @@
+class CreateUser < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user do |t|
+      t.type][ :[field[
+      t.type][ :field[
+      t.string :[options]
+
+      t.timestamps
+    end
+  end
+end
