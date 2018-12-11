7399032f4242ef67703dd587f12ec5f77b3c55fa
nomadriding <fundora.a@gmail.com>
2018-12-11 16:15:30 -0500

    add itinerary scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_234735) do
+ActiveRecord::Schema.define(version: 2018_12_11_211353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "itineraries", force: :cascade do |t|
+    t.string "start_date"
+    t.string "end_date"
+    t.integer "available_seat"
+    t.string "projected_eta"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_itineraries_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -28,4 +40,5 @@ ActiveRecord::Schema.define(version: 2018_12_10_234735) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "itineraries", "users"
 end
