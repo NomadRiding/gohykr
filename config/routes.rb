dc3bd08ad0238f015e23238033321702ae9806ac
nomadriding <fundora.a@gmail.com>
2018-12-10 19:53:05 -0500

    reroute localhost to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'hello_world#index'
 end
