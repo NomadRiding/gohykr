a7afd2e2fe088fd75ccacecd666d9d1d5c309874
Maya Kano <mayakanoweb@gmail.com>
2018-12-17 11:24:24 -0500

    changes to the layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :password_digest, presence: true, length: { minimum: 3, maximum: 15 },
+  uniqueness: { case_sensitive: false }  
   has_secure_password
 
 
