d22b5de579918419f419e6a5028ee035d7e1976d
Maya Kano <mayakanoweb@gmail.com>
2018-12-19 18:45:13 -0500

    working chatroom that only works when logged in, saves your conversations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,12 @@ class ApplicationController < ActionController::Base
   def current_user
     User.find(session[:user_id])
   end
+
+  def require_user
+    if !logged_in?
+      redirect_to login_path
+    end
+  end
+
   helper_method :current_user
 end
