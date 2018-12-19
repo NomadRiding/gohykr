e42ea4b9d279e3525feaa3a85f748c3cbf91f176
Maya Kano <mayakanoweb@gmail.com>
2018-12-19 13:28:57 -0500

    env file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_194956) do
+ActiveRecord::Schema.define(version: 2018_12_17_202017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2018_12_17_194956) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.text "content"
+    t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "itineraries", force: :cascade do |t|
     t.string "start_date"
     t.string "end_date"
@@ -46,6 +55,15 @@ ActiveRecord::Schema.define(version: 2018_12_17_194956) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_itineraries_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+  end
+
+  create_table "user", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
   end
 
   create_table "users", force: :cascade do |t|
