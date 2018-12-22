e6d1959b2aea5e9d1dfa040154e93c29e25df721
NomadRiding <fundora.a@gmail.com>
2018-12-22 14:34:53 -0500

    unique email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password 
+  has_secure_password
   has_many :messages
   has_one_attached :avatar_image
   has_many :itineraries
-  
+  validates :email, uniqueness: true
+
 end
