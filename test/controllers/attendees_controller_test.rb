6bab7e6ad252a0bf1c72f708f4e75ad9c36ffecb
Albert <nomadriding@gmail.com>
2018-12-23 15:17:06 -0500

    Delete attendees_controller_test.rb

--- test/controllers/attendees_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AttendeesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get attendees_create_url
-    assert_response :success
-  end
-
-end
