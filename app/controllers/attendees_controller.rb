bd568a692efe60860a70d1738d74fdcf6fa6bcd4
NomadRiding <fundora.a@gmail.com>
2018-12-23 14:51:29 -0500

    create attendees

---
+++ app/controllers/attendees_controller.rb
@@ -0,0 +1,12 @@
+class AttendeesController < ApplicationController
+  def create
+    # binding.pry
+    itinerary = Itinerary.find(params[:itinerary_id])
+    attendee = itinerary.attendees.build(user: current_user)
+    if attendee.save
+      avail_seats = itinerary.available_seat - 1
+      itinerary.update_column(:available_seat, avail_seats)
+    end
+    redirect_to itinerary_path(itinerary)
+  end
+end
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -2,5 +2,9 @@ class Itinerary < ApplicationRecord
   belongs_to :user
   has_one_attached :avatar_image
   has_many :locations
-
+  has_many :attendees
+  has_many :events,
+            foreign_key: 'user_id',
+            class_name: 'Itinerary',
+            through: :attendees
 end
