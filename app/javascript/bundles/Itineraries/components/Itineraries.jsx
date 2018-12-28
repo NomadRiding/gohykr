ff0bbababef199b49718a22306b5036ae9669f4c
NomadRiding <nomadriding@gmail.com>
2018-12-28 14:43:47 -0500

    create react itinerary data

--- app/javascript/bundles/Itineraries/components/Itineraries.jsx
+++ app/javascript/bundles/Itineraries/components/Itineraries.jsx
@@ -1,22 +1,48 @@
-// import React, { component } from 'React'
-// import axios from 'axios'
+import React, { Component } from 'react'
+import axios from 'axios'
+import TableRow from './TableRow'
 
+export default class Itineraries extends Component {
+  state = {
+    itineraries: [{}, {}, {}, {}, {}]
+  }
 
-// export default class Itineraries extends Component {
-//   state = {
-//     itineraries: [ {},{},{},{},{} ]
-//   }
+  fetchItineraries = () => {
+    axios.get('/itineraries.json')
+      .then(response => {
+        const { itineraries } = response.data
+        this.setState({ itineraries })
+      })
+  }
 
-//   fetchItineraries = () => {
-//     axios.get( '/Itineraries.json')
-//     .then(response => {
-//       const { itineraries }
-//     })
-//   }
+  render() {
+    const { itineraries } = this.state
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th style={{ width: '15%' }}>Start date</th>
+            <th style={{ width: '15%' }}>Due date</th>
+            <th style={{ width: '15%' }}>Available seat</th>
+            <th style={{ width: '15%' }}>ETA</th>
+            <th style={{ width: '15%' }}>Start Loc</th>
+            <th style={{ width: '15%' }}>End Loc</th>
+            <th style={{ width: '15%' }}>Description</th>
+            <th style={{ width: '15%' }}>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            tasks.map((task, index) => {
+              return <TableRow key={index} {...task} />
+            })
+          }
+        </tbody>
+      </table>
+    )
+  }
 
-//   render() {
-//     return(
-//       <h1> hello this is a place holder </h1>
-//     )
-//   }
-// }
+  componentDidMount() {
+    this.fetchItineraries()
+  }
+}
