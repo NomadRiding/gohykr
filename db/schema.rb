bf4a8ceca83a750e25012cfa7136bd688f9073d3
Jerry Trujillo <trujillojerry70@yahoo.com>
2018-12-29 17:10:29 -0500

    remove profile pic column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_203907) do
+ActiveRecord::Schema.define(version: 2018_12_29_220805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(version: 2018_12_22_203907) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "profile_picture"
   end
 
   add_foreign_key "attendees", "itineraries"
