aa2720f506ae70f1ab2948d56dbb55ba71e5648e
Maya Kano <mayakanoweb@gmail.com>
2018-12-29 19:01:30 -0500

    changes to post a ride

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     unless @current_user = User.find(session[:user_id]) == nil
-         @current_user = User.find(session[:user_id])
+      @current_user = User.find(session[:user_id])
     else
       @current_user = nil
     end
