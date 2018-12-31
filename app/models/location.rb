340486fed16692a7df9be5aeae94efb6ac539a8b
NomadRiding <nomadriding@gmail.com>
2018-12-30 19:45:32 -0500

    comment out unneccessary code

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,12 +3,12 @@ class Location < ApplicationRecord
   scope :origin, -> { find_by_is_origin(true) }
   scope :destination, -> { find_by_is_origin(false) }
 
-  def get_coords
-    results = Geocoder.search( self.address )
-    self.latitude = results[0].data["lat"]
-    self.longitude = results[0].data["lon"]
+  # def get_coords
+  #   results = Geocoder.search( self.address )
+  #   self.latitude = results[0].data["lat"]
+  #   self.longitude = results[0].data["lon"]
 
-    self.save
+  #   self.save
 
-  end
+  # end
 end
